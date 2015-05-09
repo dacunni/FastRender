ad0f2ce7d1df49c2a64f1d28e08d397e9096541d
David Cunningham <dacunni@gmail.com>
2015-05-08 23:43:17 -0600

    minor compile fix

--- RandomNumberGenerator.hpp
+++ RandomNumberGenerator.hpp
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 inline float RandomNumberGenerator::uniform01Impl( void )
 {
---
+++ test_render.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <algorithm>
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <math.h>
+
+#define MAGICKCORE_QUANTUM_DEPTH 16
+#define MAGICKCORE_HDRI_ENABLE 0
+#include <Magick++.h>
+
+#include "Matrix.h"
+#include "Transform.h"
+#include "Ray.h"
+#include "Sphere.h"
+#include "RandomNumberGenerator.h"
+#include "Timer.h"
+
+RandomNumberGenerator rng;
+std::string output_path = "testoutput";
+
+int main (int argc, char * const argv[]) 
+{
+    printf("Render Tests\n");
+    fflush(stdout);
+
+    mkdir(output_path.c_str(), 0777);
+
+    Timer total_run_timer;
+    total_run_timer.start();
+
+    rng.seedCurrentTime();
+
+    // Tests
+    
+    total_run_timer.stop();
+    printf("Done - Run time = %f seconds\n", total_run_timer.elapsed());
+    fflush(stdout);
+    return 0;
+}
+
