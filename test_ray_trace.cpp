dacunni/FastRender

commit 09f65185b258e005c665c70b63d09b60097395b4

Added ray logging. Added refractiont tests for sphere and box

Scene.cpp
@@ -33,6 +33,8 @@ void Scene::updateAnim( float t )
 
 bool Scene::intersect( const Ray & ray, RayIntersection & intersection ) const
 {
+    bool hit = false;
+
     // Asserts
     ray.direction.assertIsUnity();
     ray.direction.assertIsDirection();
@@ -61,17 +63,19 @@ bool Scene::intersect( const Ray & ray, RayIntersection & intersection ) const
         }
         intersection.normal.normalize();
         intersection.normal.makeDirection();
-        return true;
+        hit =  true;
 	}
 
-	if( env_map ) {
+	if( !hit && env_map ) {
         intersection.sample = env_map->sample( ray );
         intersection.ray = ray;
         intersection.distance = FLT_MAX;
-        return true;
+        hit = false;
     }
 
-    return false;
+    logIntersect( ray, intersection, hit );
+
+    return hit;
 }
 
 bool Scene::intersectsAny( const Ray & ray, float min_distance ) const
@@ -120,3 +124,13 @@ void Scene::addLightsForTraceable( std::shared_ptr<Traceable> obj )
     }
 }
 
+inline void Scene::logIntersect( const Ray & ray, const RayIntersection & intersection, bool hit ) const
+{
+    if( trace_log ) {
+        TraceLog::Entry entry;
+        entry.ray = ray;
+        entry.intersection = intersection;
+        entry.hit = hit;
+        trace_log->entries.push_back( entry );
+    }
+}

test_ray_trace.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <memory>
 #include <algorithm>
+#include <functional>
 
 #include <sys/stat.h>
 #include <stdio.h>
@@ -332,6 +333,101 @@ void testRefractAnglesEqualIndex()
     testRefractForIndices( plot, 1.2, 1.2 );
 }
 
+
+enum LightSourcePattern { PATTERN_RADIAL, PATTERN_PARALLEL };
+void testRefractObject(std::shared_ptr<Traceable> obj,
+                       const std::string & plot_file_name,
+                       std::function<void(Plot2D&)> plot_obj,
+                       LightSourcePattern light_pattern)
+{
+    Plot2D plot( output_path + "/" + plot_file_name, 1000, 1000,
+                 -2.0, 4.0, -3.0, 3.0 );
+    Scene scene;
+    Vector4 o( -1, 1, 0 ), d;
+    auto at = Vector4( 0.0, 0.0, 0.0 );
+
+    plot.fillColor( 0.8, 0.8, 1.0 );
+    plot.strokeColor( 0.4, 0.4, 1.0 );
+    plot_obj( plot );
+
+    obj->material = std::make_shared<RefractiveMaterial>(N_DIAMOND);
+    scene.root = obj;
+
+    TraceLog log; scene.startLogging( &log );
+    BasicDiffuseSpecularShader shader;
+
+    float radius = 1.5;
+    int nsteps = 35;
+    float alpha = 0.0;
+
+    for( int i = 1; i <= nsteps; i++ ) {
+        log.entries.clear();
+
+        if( light_pattern == PATTERN_RADIAL ) {
+            alpha = (float) i / (nsteps + 2);
+            float angle = (1.0 - alpha) * M_PI + alpha * M_PI / 2.0;
+            o.x = radius * cos(angle);
+            o.y = radius * sin(angle);
+            d = subtract( at, o ).normalized();
+        }
+        else if( light_pattern == PATTERN_PARALLEL ) {
+            alpha = (float) i / nsteps;
+            o = blend( Vector4( -1.5, 0, 0 ), 1.0 - alpha, Vector4( 0, 1.5, 0 ), alpha );
+            d = Vector4( 1, -1, 0 ).normalized();
+        }
+
+        Ray ray( o, d );
+        RayIntersection intersection;
+
+        bool hit = scene.intersect( ray, intersection );
+
+        if( hit ) {
+            if( intersection.distance != FLT_MAX ) {
+                shader.shade( scene, rng, intersection );
+            }
+        }
+
+        for( int i = 0; i < (int) log.entries.size(); i++ ) {
+            auto & entry = log.entries[i];
+            auto & o = entry.ray.origin;
+            auto & d = entry.ray.direction;
+            plot.strokeColor( alpha, 0.0, 1.0 - alpha );
+            plot.fillColor( alpha, 0.0, 1.0 - alpha );
+            plot.drawCircle( o.x, o.y, 0.03 );
+            plot.drawLine( o, add( o, scale( d, 0.4 ) ) );
+        }
+    }
+}
+
+void testRefractAxisAlignedSlab()
+{
+    auto obj = std::make_shared<AxisAlignedSlab>( -1.0, -1.5, -2.0,
+                                                   3.0,  0.0,  2.0 );
+    auto plot_obj = [obj](Plot2D & plot) {
+        plot.drawRect( obj->xmin, obj->ymin, obj->xmax, obj->ymax );
+    };
+
+    testRefractObject( std::dynamic_pointer_cast<Traceable>(obj),
+                       std::string("refract_slab_parallel.png"), plot_obj, PATTERN_PARALLEL );
+    testRefractObject( std::dynamic_pointer_cast<Traceable>(obj),
+                       std::string("refract_slab_radial.png"), plot_obj, PATTERN_RADIAL );
+}
+
+void testRefractSphere()
+{
+    auto obj = std::make_shared<Sphere>( 1.0, -1.0, 0.0, 1.0 );
+
+    auto plot_obj = [obj](Plot2D & plot) {
+        plot.drawCircle( obj->center.x, obj->center.y, obj->radius );
+    };
+
+    testRefractObject( std::dynamic_pointer_cast<Traceable>(obj),
+                       std::string("refract_sphere_radial.png"), plot_obj, PATTERN_RADIAL );
+    testRefractObject( std::dynamic_pointer_cast<Traceable>(obj),
+                       std::string("refract_sphere_parallel.png"), plot_obj, PATTERN_PARALLEL );
+}
+
+
 // ------------------------------------------------------------ 
 // Camera ray generation
 // ------------------------------------------------------------ 
@@ -499,26 +595,22 @@ int main (int argc, char * const argv[])
     testRefractAnglesLowHigh();
     testRefractAnglesHighLow();
     testRefractAnglesEqualIndex();
+    testRefractAxisAlignedSlab();
+    testRefractSphere();
     testFresnelDialectric1();
     testFresnelDialectric2();
     testFresnelDialectric3();
     testFresnelDialectric4();
     testSimpleCameraNoJitter();
     testSimpleCamera();
-    testRefractAnglesLowHigh();
-    testRefractAnglesHighLow();
-    testRefractAnglesEqualIndex();
     // Timing
     testRaySphereTiming();
     testRayAxisAlignedSlabTiming();
     testRayMeshBunnyTiming();
     testRayMeshOctreeBunnyTiming();
 #else
-    //testRaySphereTiming();
-    testRayAxisAlignedSlabTiming();
-    //testRayMeshBunnyTiming();
-    //testRayMeshOctreeBunnyTiming();
-
+    testRefractAxisAlignedSlab();
+    testRefractSphere();
 #endif
     
     total_run_timer.stop();

BasicDiffuseSpecularShader.cpp
@@ -31,8 +31,7 @@ void BasicDiffuseSpecularShader::shade( Scene & scene, RandomNumberGenerator & r
     new_ray.depth = intersection.ray.depth + 1;
     const unsigned char max_depth = 5;
     //const unsigned char max_depth = 4;
-    //const unsigned char max_depth = 2; // TEMP
-    //const unsigned char max_depth = 1; // TEMP
+    //const unsigned char max_depth = 3;
 
     Vector4 from_dir = intersection.ray.direction;
     from_dir.negate();

Scene.h
@@ -11,10 +11,9 @@
 #define _SCENE_H_
 
 #include <vector>
+#include "Ray.h"
 
 class Traceable;
-class Ray;
-class RayIntersection;
 class EnvironmentMap;
 
 // TEMP >>> move this to its own file
@@ -33,6 +32,20 @@ public:
 };
 // TEMP <<<
 
+class TraceLog
+{
+    public:
+        class Entry
+        {
+            public:
+                Ray ray;
+                RayIntersection intersection;
+                bool hit;
+        };
+
+        std::vector<Entry> entries;
+};
+
 class Scene 
 {
 public:
@@ -50,6 +63,10 @@ public:
     // Helper for buildLightList
     void addLightsForTraceable( std::shared_ptr<Traceable> obj );
 
+    void startLogging( TraceLog * log ) { trace_log = log; }
+    void stopLogging() { trace_log = nullptr; }
+    void logIntersect( const Ray & ray, const RayIntersection & intersection, bool hit ) const;
+
     std::shared_ptr<Traceable> root;
     std::shared_ptr<EnvironmentMap> env_map;
 
@@ -62,6 +79,8 @@ public:
     // can consider them explicitely.
     std::vector<PointLight> point_lights;
 
+    // Logging
+    mutable TraceLog * trace_log = nullptr;
 };
 
 

commit d1b0310d15fbe821f7be9d1b7f74bb1cdf4bb3b7

Speed up intersectsAny for AxisAlignedSlab, helping with bounded meshes Fix hit result bug introduced in 09f65185

AxisAlignedSlab.cpp
@@ -96,7 +96,7 @@ const int LOOK_NEGATIVE_NORMAL_OFFSET = 1;
 //
 // Note: This method assumes that min/max values are ordered correctly. Call correctMinMax() to ensure this
 //       prior to calling intersect().
-bool AxisAlignedSlab::intersect( const Ray & ray, RayIntersection & intersection ) const
+inline bool AxisAlignedSlab::intersectHelper( const Ray & ray, RayIntersection & intersection, bool any_test ) const
 {
     float xn, xf, yn, yf, zn, zf;       // near and far planes for the box
     int nin, nif, nix, niy, niz;        // indices into normal table (near plane, far plane, x, y, z)
@@ -190,26 +190,42 @@ bool AxisAlignedSlab::intersect( const Ray & ray, RayIntersection & intersection
 #endif
         
     if( tn > intersection.min_distance ) {
-        intersection.normal = boxNormals[ nin ];
-        intersection.distance = tn;
-        intersection.ray = ray;
-		scale( ray.direction, intersection.distance, intersection.position );
-		add( intersection.position, ray.origin, intersection.position );
-        intersection.material = material;
+        if( !any_test ) {
+            intersection.normal = boxNormals[ nin ];
+            intersection.distance = tn;
+            intersection.ray = ray;
+            scale( ray.direction, intersection.distance, intersection.position );
+            add( intersection.position, ray.origin, intersection.position );
+            intersection.material = material;
+        }
         return true;
     }
     else if( tf > intersection.min_distance ) {
-        intersection.normal = boxNormals[ nif ];
-        intersection.distance = tf;
-        intersection.ray = ray;
-		scale( ray.direction, intersection.distance, intersection.position );
-		add( intersection.position, ray.origin, intersection.position );
-        intersection.material = material;
+        if( !any_test ) {
+            intersection.normal = boxNormals[ nif ];
+            intersection.distance = tf;
+            intersection.ray = ray;
+            scale( ray.direction, intersection.distance, intersection.position );
+            add( intersection.position, ray.origin, intersection.position );
+            intersection.material = material;
+        }
         return true;
     }
     else {
         return false;
     }
+}
+
+bool AxisAlignedSlab::intersect( const Ray & ray, RayIntersection & intersection ) const
+{
+    return intersectHelper( ray, intersection, false );
+}
+
+bool AxisAlignedSlab::intersectsAny( const Ray & ray, float min_distance ) const
+{
+    RayIntersection intersection;
+    intersection.min_distance = min_distance;
+    return intersectHelper( ray, intersection, true );
 }
 
 void AxisAlignedSlab::print( FILE * file ) const

AxisAlignedSlab.h
@@ -31,6 +31,7 @@ public:
     inline float volume() const { return xdim() * ydim() * zdim(); }
     
     virtual bool intersect( const Ray & ray, RayIntersection & intersection ) const;
+	virtual bool intersectsAny( const Ray & ray, float min_distance ) const;
 
     float xmin, ymin, zmin;
     float xmax, ymax, zmax;
@@ -38,6 +39,9 @@ public:
     static unsigned long intersection_test_count;       // Counts the number of intersection tests against
                                                         // objects of this class
     virtual void print( FILE * file = stdout ) const;
+
+private:
+    inline bool intersectHelper( const Ray & ray, RayIntersection & intersection, bool any_test ) const;
 };
 
 AxisAlignedSlab merge( const AxisAlignedSlab & a, const AxisAlignedSlab & b );

Scene.cpp
@@ -70,7 +70,7 @@ bool Scene::intersect( const Ray & ray, RayIntersection & intersection ) const
         intersection.sample = env_map->sample( ray );
         intersection.ray = ray;
         intersection.distance = FLT_MAX;
-        hit = false;
+        hit = true;
     }
 
     logIntersect( ray, intersection, hit );

test_materials.cpp
@@ -237,7 +237,7 @@ int main (int argc, char * const argv[])
     rng.seedCurrentTime();
 
     // Tests
-#if 1
+#if 0
     // Run all tests
     printTests();
     runTests();
@@ -256,7 +256,7 @@ int main (int argc, char * const argv[])
     MaterialTestDiffuseWhiteHDREnvironmentMap::run(); // TODO - Needs work, probably importance sampling
     MaterialTestMirrorHDREnvironmentMap::run(); // TODO - Needs work, probably importance sampling
 #else
-    //MaterialTestRefractWaterArcLight::run();
+    MaterialTestRefractWaterArcLight::run();
 #endif
     
     total_run_timer.stop();

test_ray_trace.cpp
@@ -496,7 +496,7 @@ void testSimpleCamera()
 // ------------------------------------------------------------ 
 // Timing
 // ------------------------------------------------------------ 
-void testRayObjectTiming(Traceable & traceable, const char * name)
+void testRayObjectTiming(Traceable & traceable, const char * name, bool test_any = false)
 {
     std::vector<Ray> rays;
     int ray_pool_size = 1000;
@@ -514,15 +514,18 @@ void testRayObjectTiming(Traceable & traceable, const char * name)
     }
 
     RayIntersection isect;
+    bool hit;
 
     Timer timer;
     timer.start();
     for( int i = 0; i < num_rays; i++ ) {
         int j = i % ray_pool_size;
-        bool hit = traceable.intersect( rays[j], isect );
-        // TEMP >>>
-        //bool hit = traceable.intersectsAny( rays[j], 0.01 );
-        // TEMP <<<
+        if( test_any ) {
+            hit = traceable.intersectsAny( rays[j], 0.01 );
+        }
+        else {
+            hit = traceable.intersect( rays[j], isect );
+        }
         if( i % 1000 == 0
             && timer.elapsed() > timer_cutoff ) {
             num_rays = i + 1;
@@ -532,20 +535,22 @@ void testRayObjectTiming(Traceable & traceable, const char * name)
     timer.stop();
     float elapsed = timer.elapsed();
     float rays_per_second = (float) num_rays / elapsed;
-    printf("%s : %d rays in %f seconds = %.2f rays / sec\n",
-           name, num_rays, elapsed, rays_per_second);
+    printf("%s (any=%d) : %d rays in %f seconds = %.2f rays / sec\n",
+           name, (int)test_any, num_rays, elapsed, rays_per_second);
 }
 
 void testRaySphereTiming()
 {
     Sphere sphere( 0, 0, 0, 1 );
-    testRayObjectTiming( sphere, "Sphere" );
+    testRayObjectTiming( sphere, "Sphere", false );
+    testRayObjectTiming( sphere, "Sphere", true );
 }
 
 void testRayAxisAlignedSlabTiming()
 {
     AxisAlignedSlab slab( -1, -1, -1, 1, 1, 1 );
-    testRayObjectTiming( slab, "AxisAlignedSlab" );
+    testRayObjectTiming( slab, "AxisAlignedSlab", false );
+    testRayObjectTiming( slab, "AxisAlignedSlab", true );
 }
 
 void testRayMeshBunnyTiming()
@@ -588,7 +593,7 @@ int main (int argc, char * const argv[])
     rng.seedCurrentTime();
 
     // Tests
-#if 1
+#if 0
     testRayIntersect();
     testReflectAngles();
     testSnellAngles();
@@ -609,8 +614,9 @@ int main (int argc, char * const argv[])
     testRayMeshBunnyTiming();
     testRayMeshOctreeBunnyTiming();
 #else
-    testRefractAxisAlignedSlab();
-    testRefractSphere();
+    //testRaySphereTiming();
+    testRayAxisAlignedSlabTiming();
+    testRayMeshOctreeBunnyTiming();
 #endif
     
     total_run_timer.stop();

test_render.cpp
@@ -2302,7 +2302,10 @@ int main (int argc, char * const argv[])
     CSGLogicalANDLensCrownGlass::run();
     CSGLogicalANDLensFlintGlass::run();
 #else
-    testSphereLight1();
+    //testSphereLight1();
+    //testMesh1();         // Stanford Bunny and Dragon
+    //testAreaLight1();   // Cube with area light
+    testLogicalAND();
 
 #endif
     