1a34f45d86954c27c81c9a8587c150f619e94e75
David Cunningham <dacunni@gmail.com>
2017-06-01 22:57:02 -0600

    reduced ray count on gooch test to speed it up

--- test_materials.cpp
+++ test_materials.cpp
@@ -73,7 +73,7 @@ END_SCENE()
 BEGIN_DERIVED_SCENE(MaterialTestGooch, MaterialTestBase)
 SETUP_SCENE(
     MaterialTestBase::setup();
-    tracer->rays_per_pixel = 30;
+    tracer->rays_per_pixel = 10;
     tracer->shader = new GoochShader();
 );
 BUILD_SCENE(
