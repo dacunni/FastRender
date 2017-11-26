28f4b6017f33cf1cb317fdfb309fd47bdf30d8f0
David Cunningham <dacunni@gmail.com>
2017-11-25 22:46:12 -0700

    Added operators to add and multiply colors

--- Color.h
+++ Color.h
@@ -20,6 +20,7 @@
 class RGBColor {
     public:
     RGBColor( float red, float green, float blue ) : r(red), g(green), b(blue) {}
+    RGBColor( float whiteness ) : r(whiteness), g(whiteness), b(whiteness) {}
     RGBColor( const RGBColor & c ) : r(c.r), g(c.g), b(c.b) {}
     RGBColor() : r(0.0f), g(0.0f), b(0.0f) {}
     ~RGBColor() {}
@@ -43,8 +44,14 @@ class RGBColor {
     };
 };
 
-inline RGBColor mult( const RGBColor & a, const RGBColor & b ) {
-    return RGBColor( a.r * b.r, a.g * b.g, a.b * b.b );
-}
+inline RGBColor mult( const RGBColor & a, const RGBColor & b ) { return a.scaled(b.r, b.g, b.b); }
+inline RGBColor mult( const RGBColor & a, float s ) { return a.scaled(s); }
+inline RGBColor mult( float s, const RGBColor & a ) { return a.scaled(s); }
+
+inline RGBColor operator+( const RGBColor & a, const RGBColor & b )
+    { return RGBColor(a.r + b.r, a.g + b.g, a.b + b.b); }
+inline RGBColor operator*( const RGBColor & a, const RGBColor & b ) { return a.scaled(b.r, b.g, b.b); }
+inline RGBColor operator*( const RGBColor & a, float s ) { return a.scaled(s); }
+inline RGBColor operator*( float s, const RGBColor & a ) { return a.scaled(s); }
 
 #endif
