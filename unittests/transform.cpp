c25dc050f1cf62c04cc91c748ee85cfda7734559
David Cunningham <dacunni@gmail.com>
2017-12-01 21:22:57 -0700

    added scaling transform unit test

--- unittests/transform.cpp
+++ unittests/transform.cpp
@@ -41,6 +41,26 @@ TEST(BasicTransformTest, Rotation) {
     EXPECT_FLOAT_EQ(dot(mult(t.fwd, a), Vector4(a.y, -a.x, a.z)), a.magnitude_sq());
 }
 
+TEST(BasicTransformTest, Scaling) {
+    Transform t;
+    Vector4 a, b;
+
+    a.set(5, 49, -45);
+    float sx = 3.2, sy = -4.3, sz = 237.0;
+    t = makeScaling(sx, sy, sz);
+    b.set(a.x * sx, a.y * sy, a.z * sz);
+    EXPECT_EQ(b, mult(t.fwd, a));
+    b.set(a.x / sx, a.y / sy, a.z / sz);
+    EXPECT_EQ(b, mult(t.rev, a));
+
+    float s = 4.5;
+    t = makeScaling(s);
+    b.set(a.x * s, a.y * s, a.z * s);
+    EXPECT_EQ(b, mult(t.fwd, a));
+    b.set(a.x / s, a.y / s, a.z / s);
+    EXPECT_EQ(b, mult(t.rev, a));
+}
+
 } // namespace
 
 int main(int argc, char **argv) {
