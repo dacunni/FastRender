b55c0520763d84f25d8e4e439729144f658bed3c
David Cunningham <dacunni@gmail.com>
2018-07-21 11:46:08 -0600

    Removed duplicate code from DirectPointLightShader

--- DirectPointLightShader.cpp
+++ DirectPointLightShader.cpp
@@ -11,18 +11,7 @@
 
 void DirectPointLightShader::shade( Scene & scene, RandomNumberGenerator & rng, RayIntersection & intersection )
 {
-    RGBColor totalRadiance;
-
-    for( const PointLight & light : scene.point_lights ) {
-        Vector4 toLight = subtract( light.position, intersection.position );
-        Vector4 direction = toLight.normalized();
-        direction.makeDirection();
-
-        RGBColor Li = samplePointLight( scene, intersection, light );
-        totalRadiance += reflectedRadiance(intersection, Li, direction);
-    }
-
-    intersection.sample.color = totalRadiance;
+    intersection.sample.color = samplePointLights(scene, intersection);
 }
 
 
